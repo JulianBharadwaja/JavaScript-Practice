7edd7683bc5a3038aa8696585b878ed7a3e5b1c6
Julian Bharadwaja <julianbharadwaja1234@gmail.com>
2021-06-30 11:57:19 +0630

    variable branch complete

---
+++ 1_JS_Intro/2_variables.js
@@ -0,0 +1,29 @@
+var firstNum = 10;
+console.log(firstNum);
+
+var secondNum = 15;
+console.log(secondNum);
+
+// Camel case (Recommended for JS)
+var firstNume;
+// Pascal case
+var FirstNum;
+// Snake case
+var first_num;
+
+// Rules for naming JS variables.
+
+// - Name can contain letters, digits, _ and $
+var mySubject1, math$;
+
+// - Name cannot start with a digit
+// var 1math;
+
+// - Name can start with an _ or $
+var _subjectmarks, $rankInClass;
+
+// - Names are case sensitive
+var x, X;
+
+// - Names cannot be reserved keywords
+// var var
---
+++ 1_JS_Intro/3_JS_Quiz.js
@@ -0,0 +1,14 @@
+// Q. Which of these variables have valid names and which variables have invalid names?
+// Give reason.
+// 1. &alpha1
+// 2. NUM1
+// 3. DATE-5
+// 4. TotalSum_
+// 5. 12Byby
+// 6. num1
+// 7. super$$
+// 8. Object
+// 9. StringNew
+// 10.$rankInClass
+
+// Answer 1, 2, 3, 4, 6, 7, 8, 9, 10 => valid
