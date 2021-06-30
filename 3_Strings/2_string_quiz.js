63c378eced7750c1048c3c885c7a1d2f2811e867
Julian Bharadwaja <julianbharadwaja1234@gmail.com>
2021-06-30 13:27:56 +0630

    another commit

---
+++ 3_Strings/2_string_quiz.js
@@ -0,0 +1,22 @@
+// Q. Answer the following questions. Also, explain why do you think that your answer is
+// correct.
+// 1. console.log('JavaScript is Cool")
+// 2. console.log('I'm gonna learn JavaScript')
+// 3. console.log("This is a \"test\" string")
+// 4. console.log('This is another /'test/' string')
+// 5. console.log("How about this string??")
+
+// console.log('JavaScript is Cool");
+// you will get a syntax error for it since you are trying to log out the string with single and doublequote for one string. you need to use same quote for start of the string and end of its.
+
+// console.log('I'm gonna learn JavaScript')
+// you will also get a syntax error for it cause js will only notice single quote for the first I and then it will be looking for + for concatenation.
+
+// console.log("This is a \"test\" string");
+//  answer will be This is a "test" string.
+
+// console.log('This is another /'test/' string');
+// answer will be syntax error and this time it use the wrong symbol for adding quote into your string.
+
+console.log("How about this string??");
+// How about this string will be return as expected.
