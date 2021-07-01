c639d7cba0cf100130e95f1c129095b70c916c02
Julian Bharadwaja <julianbharadwaja1234@gmail.com>
2021-07-01 12:11:04 +0630

    commit again

---
+++ 5_Conditional_Statement/3_switch.js
@@ -0,0 +1,12 @@
+var currentDay = "Saturday";
+
+switch(currentDay) {
+    case "Monday": console.log('Timings: 10:00-06:00');break;
+    case "Tuesday": console.log('Timings: 11:00-06:00');break;
+    case "Wednesday": console.log('Timings: 12:00-06:00');break;
+    case "Thursday": console.log('Timings: 9:00-06:00');break;
+    case "Friday": console.log('Timings: 8:00-06:00');break;
+    case "Saturday": console.log('Timings: 6:00-06:00');break;
+    case "Sunday": console.log('Timings: 7:00-06:00');break;
+    default: console.log("wrong date");break;
+}
