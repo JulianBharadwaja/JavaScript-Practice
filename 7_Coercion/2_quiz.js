855a77a9c855cc0efdfdfb3eedeba6a2b8cd6306
Julian Bharadwaja <julianbharadwaja1234@gmail.com>
2021-07-01 16:43:32 +0630

    branch 7 has been added

---
+++ 7_Coercion/2_quiz.js
@@ -0,0 +1,11 @@
+// Q1. What will be printed in the console?
+console.log(true + false) //1
+console.log(12 / "6") //2
+console.log("number" + 15 + 3) // "number153"
+console.log(15 + 3 + "number") // "18number"
+console.log(1 > null) // true
+console.log("foo" + + "bar") // fooNaN
+console.log('true' == true)// false
+console.log(false == 'false') // false
+console.log(null == '') // false
+console.log(!!"false" == !!"true") // true
---
+++ 7_Coercion/3_explicit.js
@@ -0,0 +1,20 @@
+// Convert anything to string
+var mNum = 10;
+console.log(typeof(String(mNum)));
+
+// To Number
+console.log(typeof(Number('3')));
+console.log(typeof(Number(true)));
+console.log(typeof(Number(false)));
+console.log((Number(null)));
+console.log(typeof(Number('-90.99')));
+console.log(typeof(Number('apple')));
+
+// To Boolean
+console.log(typeof(Boolean(' ')));
+console.log(typeof(Boolean('')));
+console.log(typeof(Boolean('a')));
+console.log(typeof(Boolean(1)));
+console.log(typeof(Boolean(0)));
+console.log(typeof(Boolean(null)));
+console.log(typeof(Boolean(undefined)));
